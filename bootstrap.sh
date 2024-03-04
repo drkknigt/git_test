14a6ac004b871dbdb33f0e4659ba2e53de53b042
drkknght <vitobnd79@gmail.com>
2024-03-04 17:00:15 +0530

    changed bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,14 +7,14 @@ if [ "$continue_script" = "yes" ]; then
 sudo pacman -S git ansible reflector
 # sudo apt install --yes ansible git
 if [ ! "1" -gt "$#" ] ; then
-    ansible-pull --extra-vars install_wifi="$wifi_info" -U https://github.com/drkknigt/arch-pull -vvv --ask-vault-pass --ask-become-pass -t "$(echo "$@" | tr " " ",")" --ask-pass
+    ansible-pull --extra-vars install_wifi="$wifi_info" -U https://github.com/drkknigt/git_test -vvv --ask-vault-pass --ask-become-pass -t "$(echo "$@" | tr " " ",")" --ask-pass
     exit
 fi
 #
 if [ "$wifi_info" = "yes" ]; then
-    ansible-pull --extra-vars install_wifi="$wifi_info" -U https://github.com/drkknigt/arch-pull -vvv --ask-become-pass --ask-pass
+    ansible-pull --extra-vars install_wifi="$wifi_info" -U https://github.com/drkknigt/git_test -vvv --ask-become-pass --ask-pass
 else
-    ansible-pull --extra-vars install_wifi="no" -U https://github.com/drkknigt/arch-pull -vvv --ask-become-pass --ask-pass
+    ansible-pull --extra-vars install_wifi="no" -U https://github.com/drkknigt/git_test -vvv --ask-become-pass --ask-pass
 fi
 . ~/.dotfiles/sys_d/systemd-disabled
 fi
